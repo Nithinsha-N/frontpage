4c9160256cbc3b3244e72b45c1826096c99d56f5
nithinsha99 <nithinshacoder99@gmail.com>
2024-01-22 11:48:34 +0530

    first change

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:project1/utils/responsivelayout.dart';
+import 'widgets/navbar.dart';
+
+import 'widgets/search.dart';
+
+void main() => runApp(MaterialApp(
+  title: 'Flutter Landing Page',
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+    primarySwatch: Colors.blue,
+  ),
+  home: HomePage(),
+));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0xFFF8FBFF),
+            Color(0xFFFCFDFD),
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[NavBar(), Body()],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayout(
+      largeScreen: LargeChild(),
+      mediumScreen: LargeChild(),
+      smallScreen: SmallChild(), key: null,
+    );
+  }
+}
+
+class LargeChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 600,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          FractionallySizedBox(
+            alignment: Alignment.centerRight,
+            widthFactor: .4,
+            heightFactor: .8,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 50,),
+              child: Image.network("assets/images/pilot.jpg", scale: .85,),
+            ),
+          ),
+          FractionallySizedBox(
+            alignment: Alignment.centerLeft,
+            widthFactor: .6,
+            child: Padding(
+              padding: EdgeInsets.only(left: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Hello!",
+                      style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Roboto",
+                          color: Color(0xFF8591B0))),
+                  RichText(
+                    text: TextSpan(
+                        text: "WellCome To ",
+                        style:
+                        TextStyle(fontSize: 60, color: Color(0xFF8591B0)),
+                        children: [
+                          TextSpan(
+                              text: "ADYNAMICS",
+                              style: TextStyle(
+                                  fontSize: 60,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87))
+                        ]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0, top: 20),
+                    child: Text("LET’S EXPLORE THE EXAM"),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Search()
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SmallChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Hello!",
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Color(0xFF8591B0),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Montserrat-Regular"),
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'WellCome To ',
+                style: TextStyle(fontSize: 40, color: Color(0xFF8591B0)),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Adynamics',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Colors.black87)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, top: 20),
+              child: Text("LET’S EXPLORE THE EXAM"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Image.asset(
+                "assets/images/6841.jpg",
+                scale: 1,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Search(),
+            SizedBox(
+              height: 30,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Appcolors{
+  static Color primary=Colors.orange;
+
+}
